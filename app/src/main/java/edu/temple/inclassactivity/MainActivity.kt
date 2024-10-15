d38cbbfef120df6bba0f850cfc5454811f229d4d
tun19502 <johnlawall24@gmail.com>
2024-10-15 12:13:48 -0400

    done

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -1,19 +1,43 @@
 package edu.temple.inclassactivity
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 
+const val IMG_KEY = "string"
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
+
 
         // Fetch images into IntArray called imageArray
         val typedArray = resources.obtainTypedArray(R.array.image_ids)
         val imageArray = IntArray(typedArray.length()) {typedArray.getResourceId(it, 0)}
         typedArray.recycle()
 
+
         // Attach an instance of ImageDisplayFragment using factory method
+        val fragment1 = ImageDisplayFragment.newInstance(imageArray)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainerView, fragment1)
+            .commit()
 
     }
+
+
+
+
+
 }
